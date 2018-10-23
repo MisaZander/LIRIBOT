5b28a34d999c753516b1b8e17fde4d231662fc1e
Alexander Osborn <misterzander@gmail.com>
2018-10-23 18:30:04 -0500

    Flanagle the bandsintown date

--- bandsintown.js
+++ bandsintown.js
@@ -1,5 +1,5 @@
 var BITObj = {
-    moment: require("moment"),
+    //moment: require("moment"),
     axios: require("axios"),
     query: function(queryString) {
         var query = "https://rest.bandsintown.com/artists/" + queryString + "/events?app_id=codingbootcamp";
@@ -22,10 +22,11 @@ var BITObj = {
             // .catch(function(error) {
             //     console.log("File write error: " + error);
             // });
+            var moment = require("moment");
             for(let i = 0; i < response.data.length; i++) {
                 console.log("Venue: " + response.data[i].venue.name);
                 console.log("Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region);
-                console.log("Date: " + response.data[i].datetime);
+                console.log("Date: " + moment(response.data[i].datetime, "YYYY-MM-DDTHH:mm:ss").format("MM/DD/YYYY"));
                 console.log("---------------------------");
 
                 if(i === 4){
