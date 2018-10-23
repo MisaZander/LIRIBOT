89956d1e57713f568f494b4a8f86c8b5ff1d723c
Alexander Osborn <misterzander@gmail.com>
2018-10-23 13:24:08 -0500

    Add a switch and catch

--- liri.js
+++ liri.js
@@ -1,5 +1,38 @@
 require("dotenv").config();
 var spotify = require("./spotify.js");
+var command = process.argv[2];
+var query = process.argv[3];
 
-spotify.spotifyObj.query("We are Young");
-//spotify.spotifyObj.get();
+if(typeof command === "undefined") {
+    //If no command is entered, stop the program
+    return printHelp();
+}
+command = command.toLowerCase();
+
+switch(command) {
+    case "spotify-this-song":
+        spotify.spotifyObj.query(query);
+    break;
+    case "concert-this":
+
+    break;
+    case "movie-this":
+
+    break;
+    case "do-what-it-says":
+    
+    break;
+    default:
+        printHelp();
+    break;
+};
+
+function printHelp() {
+    console.log("Invalid command syntax. The proper syntax is:");
+    console.log("node liri.js <command> [<query>]");
+    console.log("Possible commands are:");
+    console.log("concert-this 'artist/band name'");
+    console.log("spotify-this 'song-name'");
+    console.log("movie-this 'movie-name'");
+    console.log("do-what-it-says");
+};
