8231bc21ef9d6441d377624d87b57e3443d37069
Alexander Osborn <misterzander@gmail.com>
2018-10-26 14:29:52 -0500

    Bandsintown undefined check and empty array check

--- bandsintown.js
+++ bandsintown.js
@@ -4,25 +4,31 @@ var BITObj = {
         var query = "https://rest.bandsintown.com/artists/" + queryString + "/events?app_id=codingbootcamp";
         this.request(query, function(err, response, data) {
             var moment = require("moment");
+            var fs = require("fs");
             var fdata = JSON.parse(data);
             //console.log(fdata);
+            if(fdata.length === 0) {
+                fs.appendFile("log.txt", "No events scheduled for specified query.", function(err) {
+                    
+                });
+                return console.log("No events scheduled for specified query.");                
+            }
             for(let i = 0; i < fdata.length; i++) {
-                console.log("Venue: " + fdata[i].venue.name);
-                console.log("Venue Location: " + fdata[i].venue.city + ", " + fdata[i].venue.region);
-                console.log("Date: " + moment(fdata[i].datetime, "YYYY-MM-DDTHH:mm:ss").format("MM/DD/YYYY"));
+                console.log("Venue: " + (typeof fdata[i].venue.name === "undefined" ? "Not Specified" : fdata[i].venue.name));
+                console.log("Venue Location: " + (typeof fdata[i].venue.city === "undefined" ? "Not Specified" : fdata[i].venue.city)  + ", " + (typeof fdata[i].venue.region === "undefined" ? "Not Specified" : fdata[i].venue.region));
+                console.log("Date: " + moment((typeof fdata[i].datetime === "undefined" ? "Not Specified" : fdata[i].datetime) , "YYYY-MM-DDTHH:mm:ss").format("MM/DD/YYYY"));
                 console.log("---------------------------");
 
                 if(i === 4){
                     break; //Limit to 5 results
                 }
             }
-
-            var fs = require("fs");
+            
             for(let i = 0; i < fdata.length; i++) {
                 fs.appendFile("log.txt", 
-                    "Venue: " + fdata[i].venue.name + "\n" +
-                    "Venue Location: " + fdata[i].venue.city + ", " + fdata[i].venue.region + "\n" +
-                    "Date: " + moment(fdata[i].datetime, "YYYY-MM-DDTHH:mm:ss").format("MM/DD/YYYY") + "\n-------------------------------\n",
+                    "Venue: " + (typeof fdata[i].venue.name === "undefined" ? "Not Specified" : fdata[i].venue.name)  + "\n" +
+                    "Venue Location: " + (typeof fdata[i].venue.city === "undefined" ? "Not Specified" : fdata[i].venue.city)  + ", " + (typeof fdata[i].venue.region === "undefined" ? "Not Specified" : fdata[i].venue.region)  + "\n" +
+                    "Date: " + moment((typeof fdata[i].datetime === "undefined" ? "Not Specified" : fdata[i].datetime), "YYYY-MM-DDTHH:mm:ss").format("MM/DD/YYYY") + "\n-------------------------------\n",
                     function(err) {
                         if(err) {
                             return console.log(err);
