d546a7bf4cc90f1376f056e33bf5ff0452375aac
Alexander Osborn <misterzander@gmail.com>
2018-10-26 14:11:55 -0500

    movie.js catch undefined property error

--- movie.js
+++ movie.js
@@ -11,25 +11,25 @@ var MovieObj = {
             let fdata = JSON.parse(body);
             //console.log(fdata);
 
-            console.log("Title: " + fdata.Title);
-            console.log("Release Year: " + fdata.Year);
-            console.log("IMDB Rating: " + fdata.imdbRating);
-            console.log("Rotten Tomatoes Rating: " + fdata.Ratings[1].Value);
-            console.log("Production Country: " + fdata.Country);
-            console.log("Language: " + fdata.Language);
-            console.log("Plot: " + fdata.Plot);
-            console.log("Actors: " + fdata.Actors);
+            console.log("Title: " + (typeof fdata.Title === "undefined" ? "Not Specified" : fdata.Title));
+            console.log("Release Year: " + (typeof fdata.Year === "undefined" ? "Not Specified" : fdata.Year));
+            console.log("IMDB Rating: " + (typeof fdata.imdbRating === "undefined" ? "Not Specified" : fdata.imdbRating));
+            console.log("Rotten Tomatoes Rating: " + (typeof fdata.Ratings[1] === "undefined" ? "Not Specified" : fdata.Ratings[1].Value));
+            console.log("Production Country: " + (typeof fdata.Country === "undefined" ? "Not Specified" : fdata.Country));
+            console.log("Language: " + (typeof fdata.Language === "undefined" ? "Not Specified" : fdata.Language));
+            console.log("Plot: " + (typeof fdata.Plot === "undefined" ? "Not Specified" : fdata.Plot));
+            console.log("Actors: " + (typeof fdata.Actors === "undefined" ? "Not Specified" : fdata.Actors));
 
             let fs = require("fs");
             fs.appendFile("log.txt",
-                "Title: " + fdata.Title + "\n" +
-                "Release Year: " + fdata.Year + "\n" +
-                "IMDB Rating: " + fdata.imdbRating + "\n" +
-                "Rotten Tomatoes Rating: " + fdata.Ratings[1].Value + "\n" +
-                "Production Country: " + fdata.Country + "\n" +
-                "Language: " + fdata.Language + "\n" +
-                "Plot: " + fdata.Plot + "\n" +
-                "Actors: " + fdata.Actors + "\n---------------------------\n",
+                "Title: " + (typeof fdata.Title === "undefined" ? "Not Specified" : fdata.Title) + "\n" +
+                "Release Year: " + (typeof fdata.Year === "undefined" ? "Not Specified" : fdata.Year) + "\n" +
+                "IMDB Rating: " + (typeof fdata.imdbRating === "undefined" ? "Not Specified" : fdata.imdbRating) + "\n" +
+                "Rotten Tomatoes Rating: " + (typeof fdata.Ratings[1] === "undefined" ? "Not Specified" : fdata.Ratings[1].Value) + "\n" +
+                "Production Country: " + (typeof fdata.Country === "undefined" ? "Not Specified" : fdata.Country) + "\n" +
+                "Language: " + (typeof fdata.Language === "undefined" ? "Not Specified" : fdata.Language) + "\n" +
+                "Plot: " + (typeof fdata.Plot === "undefined" ? "Not Specified" : fdata.Plot) + "\n" +
+                "Actors: " + (typeof fdata.Actors === "undefined" ? "Not Specified" : fdata.Actors) + "\n---------------------------\n",
                 function(err) {
                     if(err) return console.log("Could not write to logfile");
                 }
